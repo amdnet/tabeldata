93753713d8b1cbe017b0d1ade0f01b7d9dde2bd2
amdnet <machmudseo@gmail.com>
2021-10-14 20:53:57 +0700

    first commit

---
+++ app/Controllers/Home.php
@@ -0,0 +1,47 @@
+<?php namespace App\Controllers;
+
+use App\Models\UserDatatable;
+use Config\Services;
+
+class Home extends BaseController
+{
+    public function index()
+    {
+        $data = [
+            'title' => 'User List'
+        ];
+
+        return view('home', $data);
+    }
+
+    public function ajaxList()
+    {
+        $request = Services::request();
+        $datatable = new UserDatatable($request);
+
+        if ($request->getMethod(true) === 'POST') {
+            $lists = $datatable->getDatatables();
+            $data = [];
+            $no = $request->getPost('start');
+
+            foreach ($lists as $list) {
+                $no++;
+                $row = [];
+                $row[] = $no;
+                $row[] = $list->name;
+                $row[] = $list->email;
+                $data[] = $row;
+            }
+
+            $output = [
+                'draw' => $request->getPost('draw'),
+                'recordsTotal' => $datatable->countAll(),
+                'recordsFiltered' => $datatable->countFiltered(),
+                'data' => $data
+            ];
+
+            echo json_encode($output);
+        }
+    }
+
+}
